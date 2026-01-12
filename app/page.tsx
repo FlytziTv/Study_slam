cb9626aaf364b06d7db7af4fa39a053900b08b3a
dealexis <flytzitv@gmail.com>
2026-01-12 20:15:03 +0100

    project init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black font-sans ">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">
+          Bienvenue sur Absolute Stream
+        </h1>
+      </main>
+    </div>
+  );
+}
