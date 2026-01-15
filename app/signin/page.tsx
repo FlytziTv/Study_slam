1ea25cd628ed198dece60784774a3598faaaaf92
dealexis <flytzitv@gmail.com>
2026-01-15 15:31:27 +0100

    creation of important pages and a temporary navbar

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,13 +1,11 @@
 import Header from "@/components/navbar/Header";
 
-export default function Home() {
+export default function Signin() {
   return (
     <div className="flex flex-col min-h-screen bg-black font-sans ">
       <Header />
       <main className="flex min-h-screen w-full flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white">
-          Bienvenue sur Absolute Stream
-        </h1>
+        <h2 className="text-4xl font-bold text-white">Page Connexion</h2>
       </main>
     </div>
   );
