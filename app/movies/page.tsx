cb08490e05370e8ecb275e93a3ab23f6461f7e50
dealexis <flytzitv@gmail.com>
2026-01-15 22:08:10 +0100

    importing carousels for a visual test

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,11 +1,19 @@
+import MediaCarrousel from "@/components/carrousel/MediaCarrousel";
+import PlateformesCarrousel from "@/components/carrousel/PlateformesCarrousel";
 import Header from "@/components/navbar/Header";
+import { allMedia, films, series, plateformesData } from "@/data/test";
 
 export default function Movie() {
   return (
     <div className="flex flex-col min-h-screen bg-black font-sans ">
       <Header />
-      <main className="flex min-h-screen w-full flex-col items-center justify-center">
-        <h2 className="text-4xl font-bold text-white">Page Films</h2>
+      <main className="flex min-h-screen w-full flex-col items-center p-10 gap-10 pt-25">
+        {/* Importation de la liste des plateformes de streaming */}
+        <PlateformesCarrousel data={plateformesData} />
+        {/* Importation de la liste des médias */}
+        <MediaCarrousel data={series} title="Séries Populaires" />
+        {/* Importation de la liste des médias */}
+        <MediaCarrousel data={films} title="Films Populaires" />
       </main>
     </div>
   );
