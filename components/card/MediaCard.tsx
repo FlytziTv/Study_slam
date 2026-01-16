21a998acf053fedc6c77cfbdf265e501dcdcca62
dealexis <flytzitv@gmail.com>
2026-01-16 18:52:08 +0100

    modification of the media cards

--- components/card/MediaCard.tsx
+++ components/card/MediaCard.tsx
@@ -11,7 +11,7 @@ export default function MediaCard({
   Reco: number;
 }) {
   return (
-    <div className="w-75 h-40 rounded-[10px] relative shrink-0 ">
+    <div className="w-75 aspect-video rounded-[10px] relative shrink-0 ">
       {/* Image background */}
       <Image
         src={UrlImage}
