db9137391640083fea690550abb6bc2e4656e372
dealexis <flytzitv@gmail.com>
2026-01-17 16:47:29 +0100

    Next.js/React hydration error correction.

--- components/card/MembersCard.tsx
+++ components/card/MembersCard.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
 import { allMedia } from "@/data/test";
 import MediaVCard from "./MediaVCard";
+
+import { useRouter } from "next/navigation";
 
 type UserStats = {
   label: string;
@@ -21,57 +24,64 @@ export default function MembersCard({
   UserStats: UserStats[];
   UserFavoritesIds: string[];
 }) {
+  // router pour la navigation vers le profil
+  const router = useRouter();
+
   return (
     // lien vers le profil de l'utilisateur
-    <Link href={`/profile/${Id}`}>
-      <div className="flex flex-row gap-4 items-center p-4 bg-white/8 rounded-lg w-full">
-        <div className="relative h-35 w-35 shrink-0">
-          {/* Photo de profil de {User} */}
-          <Image
-            src={ImageUser}
-            alt={`Photo de profil de ${ImageUser}`}
-            fill
-            className="rounded-full border-2 object-cover select-none pointer-events-none"
-          />
+    <div
+      onClick={() => router.push(`/profile/${Id}`)}
+      className="flex flex-row gap-4 items-center p-4 bg-white/8 rounded-lg w-full cursor-pointer"
+    >
+      <div className="relative h-35 w-35 shrink-0">
+        {/* Photo de profil de {User} */}
+        <Image
+          src={ImageUser}
+          alt={`Photo de profil de ${ImageUser}`}
+          fill
+          className="rounded-full border-2 object-cover select-none pointer-events-none"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col items-start gap-0">
+          {/* Nom de l'utilisateur */}
+          <h1 className="text-2xl font-bold text-white">{UserName}</h1>
+          {/* Liste des statistiques de l'utilisateur */}
+          <div className="flex flex-row gap-2">
+            {UserStats?.map((stat) => (
+              <span
+                key={stat.label}
+                className="font-regular text-sm text-[#A1A1A1] hover:text-white transition-colors cursor-pointer"
+              >
+                {stat.value} {stat.label}
+              </span>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col items-start gap-0">
-            {/* Nom de l'utilisateur */}
-            <h1 className="text-2xl font-bold text-white">{UserName}</h1>
-            {/* Liste des statistiques de l'utilisateur */}
-            <div className="flex flex-row gap-2">
-              {UserStats?.map((stat) => (
-                <span
-                  key={stat.label}
-                  className="font-regular text-sm text-[#A1A1A1] hover:text-white transition-colors cursor-pointer"
-                >
-                  {stat.value} {stat.label}
-                </span>
-              ))}
-            </div>
-          </div>
 
-          {/* Liste des médias favoris de l'utilisateur */}
-          <div className="flex flex-row items-center gap-2">
-            {UserFavoritesIds?.map((mediaId) => {
-              // On cherche le média correspondant dans allMedia
-              const media = allMedia.find((m) => m.id === mediaId);
+        {/* Liste des médias favoris de l'utilisateur */}
+        <div
+          className="flex flex-row items-center gap-2"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {UserFavoritesIds?.map((mediaId) => {
+            // On cherche le média correspondant dans allMedia
+            const media = allMedia.find((m) => m.id === mediaId);
 
-              // Si le média n'existe pas, on n'affiche rien
-              if (!media) return null;
+            // Si le média n'existe pas, on n'affiche rien
+            if (!media) return null;
 
-              return (
-                <MediaVCard
-                  key={media.id}
-                  UrlImage={media.afficheV}
-                  Name={media.title}
-                  Id={media.id}
-                />
-              );
-            })}
-          </div>
+            return (
+              <MediaVCard
+                key={media.id}
+                UrlImage={media.afficheV}
+                Name={media.title}
+                Id={media.id}
+              />
+            );
+          })}
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
